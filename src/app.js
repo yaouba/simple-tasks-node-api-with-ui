44bb5be809aede8d2bd879949a6c67c3d438d0a7
yaouba <kabayyaouba@gmail.com>
2024-04-28 08:38:27 +0100

    Handle 404

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const taskRoutes = require('./routes/tasks')
 const connectDB = require('./db/connect')
+const notFound = require('./middlewares/not-found')
 require('dotenv').config()
 
 
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use(express.static('./public'))
 
 app.use('/api/v1/tasks', taskRoutes)
+app.use(notFound);
 
 
 const start = async () => {
